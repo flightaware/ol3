398730f9a9d752faee17e1821e2cc363ed339463
Daniel Reiter <semibmx@gmail.com>
2015-11-16 17:08:38 -0600

    Added externs, exports, and nocollapse declarations necessary for FA

---
+++ externs/flightaware.js
@@ -0,0 +1,21 @@
+/**
+ * @externs
+ */
+
+var ol;
+
+ol.interaction;
+ol.interaction.DragPan;
+ol.interaction.DragPan.handleUpEvent_ = function(){};
+ol.interaction.Interaction;
+ol.interaction.Interaction.pan = function(){};
+ol.interaction.Interaction.zoomWithoutConstraints = function(){};
+ol.interaction.Interaction.rotateWithoutConstraints = function(){};
+ol.interaction.Interaction.zoomByDelta = function(){};
+
+ol.control;
+ol.control.Attribution;
+ol.control.Attribution.prototype.getSourceAttributions = function(){};
+
+ol.source.Vector.prototype.loadFeatures = function(){};
+
