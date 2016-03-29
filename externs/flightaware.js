f23f1258f48beca44f1c7f6506ce60d65cd6289f
Philip Clifton <philip.clifton@flightaware.com>
2016-03-29 09:45:54 -0500

    Added ol.source.Vector.forEachFeatureInExtent to externs

--- externs/flightaware.js
+++ externs/flightaware.js
@@ -4,6 +4,7 @@
 
 var ol;
 
+// See FAMap.Util.InteractionShims
 ol.interaction;
 ol.interaction.DragPan;
 ol.interaction.DragPan.handleUpEvent_ = function(){};
@@ -13,13 +14,19 @@ ol.interaction.Interaction.zoomWithoutConstraints = function(){};
 ol.interaction.Interaction.rotateWithoutConstraints = function(){};
 ol.interaction.Interaction.zoomByDelta = function(){};
 
+// See FAMap.olExtras.Control.Attribution
 ol.control;
 ol.control.Attribution;
 ol.control.Attribution.prototype.getSourceAttributions = function(){};
 
+// loadFeatures is used in several Source classes; see FAMap.olExtras.Source.[*]
 ol.source.Vector.prototype.loadFeatures = function(){};
+// Used in FAMap.olExtras.Source.ClusterMeters
 ol.source.Vector.prototype.getFeatures = function(){};
+// Used in FAMap.olExtras.Source.Animated to correctly handle IDL wrapping
+ol.source.Vector.prototype.forEachFeatureInExtent = function(){};
 
+// Also used in FAMap.olExtras.Source.ClusterMeters
 ol.source.Cluster.prototype.loadFeatures = function(){};
 ol.source.Cluster.prototype.onSourceChange_ = function(){};
 ol.source.Cluster.prototype.cluster_ = function(){};
